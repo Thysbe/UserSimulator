5bb02dd1f690f2cb24e122f2e47b4f2f4533a9cd
plgitdev <plyssikatos@gmail.com>
2020-05-25 17:41:33 -0600

    added lambdas for user query as well as tracking query and used enum instead of dict

--- userManager.py
+++ userManager.py
@@ -1,4 +1,5 @@
 import pymongo
+from pip._internal.utils.misc import enum
 from pymongo import MongoClient
 
 
@@ -9,30 +10,46 @@ class UserManager:
         self.mydb = self.my_client["discordText"]
         self.tracked_col = self.mydb["trackedUsers"]
         self.messages_col = self.mydb["messages"]
+        self.user_query = lambda name, disc: {"username": name + "#" + disc}
+        self.user_t_query = lambda name, disc, t: {"username": name + "#" + disc, "tracking": t}
+
         print(self.my_client.list_database_names())
 
     def startListening(self, author):
         if self.userExists(author):
             print('turning on message tracking')
-            query = {"username": author.name + '#' + author.discriminator}
+            query = self.user_query(author.name, author.discriminator)
             tracking = {"$set": {"tracking": True}}
             self.tracked_col.update_one(query, tracking)
         else:
-            userDict = {"username": author.name + '#' + author.discriminator, "tracking": True, "userId": author.id}
-            self.tracked_col.insert_one(userDict)
+
+            user_enum: enumerate = enum(
+                {
+                    "username": author.name + '#' + author.discriminator,
+                    "tracking": True,
+                    "userId": author.id
+                }
+            )
+            self.tracked_col.insert_one(user_enum)
 
     def stopListening(self, author):
         if self.userExists(author):
             print('turning off message tracking')
-            query = {"username": author.name + '#' + author.discriminator}
+            query = self.user_query(author.name, author.discriminator)
             tracking = {"$set": {"tracking": False}}
             self.tracked_col.update_one(query, tracking)
         else:
-            userDict = {"username": author.name + '#' + author.discriminator, "tracking": False, "userId": author.id}
-            self.tracked_col.insert_one(userDict)
+            user_enum: enumerate = enum(
+                {
+                    "username": author.name + '#' + author.discriminator,
+                    "tracking": False,
+                    "userId": author.id
+                }
+            )
+            self.tracked_col.insert_one(user_enum)
 
     def userExists(self, author):
-        query = {"username": author.name + '#' + author.discriminator}
+        query = self.user_query(author.name, author.discriminator)
         user = self.tracked_col.find(query)
         if user.count() == 1:
             return True
@@ -41,7 +58,7 @@ class UserManager:
         print("Error")
 
     def userIsTracking(self, author):
-        query = {"username": author.name + '#' + author.discriminator, "tracking": True}
+        query = self.user_t_query(author.name, author.discriminaotr, True)
         user = self.tracked_col.find(query)
         if user.count() == 1:
             return True
@@ -58,7 +75,7 @@ class UserManager:
             print("Ignoring message")
 
     def getMessages(self, author):
-        query = {"username": author.name + '#' + author.discriminator}
+        query = self.user_query(author.name, author.discriminator)
         user = self.tracked_col.find(query)
         for message in user[0].messages:
             print(message)
